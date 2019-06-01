53c8019dea4cd06fca27547704edd4afd8b1719e
Rajeeth Somasundaram <rajeeth.somasundaram@C02ST03UGQ62.Corp.ad.ctc>
2019-06-01 17:47:27 -0400

    rajs/listing_description

--- app/src/main/java/com/t6labs/locals/ServiceActivity.java
+++ app/src/main/java/com/t6labs/locals/ServiceActivity.java
@@ -1,9 +1,7 @@
 package com.t6labs.locals;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
 
 public class ServiceActivity extends AppCompatActivity {
 
