3ab931a146029342bca0b40022f714e9ccc964e2
Max <max.belleville@hotmail.com>
2019-08-04 15:14:28 -0400

    Added fabric.

--- app/src/main/java/com/t6labs/locals/SplashActivity.java
+++ app/src/main/java/com/t6labs/locals/SplashActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -13,6 +15,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_splash);
 
         continueButton = findViewById(R.id.btn_splash_continue);
